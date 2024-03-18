dc621d3980c42784c9eb648fed8cff23d9f318d9
Grace Li <gracemxli51@gmail.com>
2024-03-18 10:03:26 -0700

    adlfj

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -7,12 +7,40 @@
 using namespace std;
 
 
-// You may add any prototypes of helper functions here
 
+// You may add any prototypes of helper functions here
+int getHeight(Node* node, int level){
+
+    if (node == NULL ){
+        return level; 
+    }
+
+     if (node->left == NULL){
+        return getHeight(node->right, level+1);
+     }
+     if (node->right == NULL){
+        return getHeight(node->left, level+1);
+     }
+     
+    int leftLevel = getHeight(node->left, level+1);
+    int rightLevel = getHeight(node->right, level+1); 
+
+    if(leftLevel==rightLevel){
+        return leftLevel;
+    }
+    return -1;
+
+
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    int treeHeight = getHeight(root, 0);
+    if(treeHeight == -1){
+        return false;
+    }
+    return true;
 
 }
 
